d5061832124eb9d79b64dc225ed372f34367233e
Thomas Hanley <tom@lytro.com>
2012-01-29 09:14:14 -0800

    moar setup

--- bin/pt2tp.rb
+++ bin/pt2tp.rb
@@ -1,4 +1,10 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'../lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'../config'))
+
 require 'rubygems'
-# require 'pivotal_tracker'
-require 'target_process'
 require 'httparty'
+require 'target_process'
+require 'conf'
+
+# puts CONFIG.inspect
--- lib/pivotal_tracker.rb
+++
@@ -1,4 +0,0 @@
-class PivotalTracker
-
-
-end
